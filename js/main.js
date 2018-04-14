4517c458253068bcf149f3c6edd08409b55438ae
Petru Zaharia <petru.zaharia@hotmail.com>
2018-04-14 16:36:57 +0300

    Fetch Bookmarks + Build Output

--- js/main.js
+++ js/main.js
@@ -38,6 +38,51 @@ function saveBookmark(e){
 
     }
 
+    // Re-fetch bookmarks
+    fetchBookmarks();
+
     // Prevent form from submitting
     e.preventDefault();
 }
+
+    // Delete bookmark
+    function deleteBookmark(url){
+        // Get bookmarks from localStorage
+        var bookmarks = JSON.parse(localStorage.getItem("bookmarks")); 
+        // Loop through bookmarks
+        for (var i=0; i < bookmarks.length; i++){
+           if(bookmarks[i].url == url){
+               // Remove from array
+               bookmarks.splice(i, 1);
+           } 
+        }
+        // Re-set back to localStorage
+        localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+
+        // Re-fetch bookmarks
+        fetchBookmarks();
+    }
+
+    // Fetch bookmarks
+    function fetchBookmarks(){
+       // Get bookmarks from localStorage
+       var bookmarks = JSON.parse(localStorage.getItem("bookmarks")); 
+
+       // Get output id
+       var bookmarksResults = document.getElementById("bookmarksResults");
+
+       // Build output
+       bookmarksResults.innerHTML = "";
+       
+       for(var i = 0; i < bookmarks.length; i++){
+        var name = bookmarks[i].name;
+        var url = bookmarks[i].url;
+
+        bookmarksResults.innerHTML += "<div class='well'>"+
+                                      "<h3>"+name+
+                                      " <a class='btn btn-default' target='_blank' href='"+url+"'>Visit</a> " +
+                                      " <a onClick='deleteBookmark(\""+url+"\")' class='btn btn-danger' href='#'>Delete</a> " +
+                                      "</h3>"+
+                                      "</div>";
+       }
+    }
